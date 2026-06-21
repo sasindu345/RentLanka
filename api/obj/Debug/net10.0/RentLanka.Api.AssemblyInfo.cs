9ef104eceb5f09fcb8d8fd01fe698cafc576d5eb
wijewardana S.J. <wijewardanasj.23@uom.lk>
2026-06-21 15:00:18 +0530

    chore: update build artifacts and project dependencies

--- api/obj/Debug/net10.0/RentLanka.Api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/RentLanka.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentLanka.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5350e9525feea0c62d327eded41615538381e63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c321a2c6c410b923f743e1e22b4fb6ab8b01fcd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentLanka.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentLanka.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
